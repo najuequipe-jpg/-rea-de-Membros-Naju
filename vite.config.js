edf97fdb85819122ef78b3b1ae29bf5af57cdb2e
Naju <naju.equipe@gmail.com>
2026-05-08 23:35:36 -0300

    chore: add vite config for multi-page build

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html'),
+        admin: resolve(__dirname, 'admin.html'),
+        player: resolve(__dirname, 'player.html'),
+      },
+    },
+  },
+});
